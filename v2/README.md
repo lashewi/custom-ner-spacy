ec5e6fcb29be6b66618970fc684972be6e0a5332
lashewi <swisum@gmail.com>
2021-03-26 20:19:33 +0530

    feat: dev branch init

---
+++ v2/README.md
@@ -0,0 +1,5 @@
+## Running the script
+
+```bash
+python3 custom-model-v2.py -m=en -o=output/ -n=1000
+```
---
+++ v2/custom-model-v2.py
@@ -0,0 +1,95 @@
+from __future__ import unicode_literals, print_function
+import pickle
+import plac
+import random
+from pathlib import Path
+import spacy
+from spacy.util import minibatch, compounding
+import wandb
+
+wandb.init(project='spacy-ner')
+
+LABEL = ['I-geo', 'B-geo', 'I-art', 'B-art', 'B-tim', 'B-nat', 'B-eve', 'O', 'I-per', 'I-tim', 'I-nat', 'I-eve', 'B-per', 'I-org', 'B-gpe', 'B-org', 'I-gpe']
+
+"""
+geo = Geographical Entity
+org = Organization
+per = Person
+gpe = Geopolitical Entity
+tim = Time indicator
+art = Artifact
+eve = Event
+nat = Natural Phenomenon
+"""
+
+with open ('spacy_dataset', 'rb') as fp:
+    NER_TRAIN_TEST_DATASET = pickle.load(fp)
+
+random.shuffle(NER_TRAIN_TEST_DATASET)
+
+TRAIN_DATA = NER_TRAIN_TEST_DATASET[:38300]
+TEST_DATA = NER_TRAIN_TEST_DATASET[38300:]
+
+@plac.annotations(
+    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    new_model_name=("New model name for model meta.", "option", "nm", str),
+    output_dir=("Optional output directory", "option", "o", Path),
+    n_iter=("Number of training iterations", "option", "n", int))
+
+
+def main(model=None, new_model_name='new_model', output_dir=None, n_iter=10):
+    if model is not None:
+        nlp = spacy.load(model) 
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.blank('en')
+        print("Created blank 'en' model")
+    if 'ner' not in nlp.pipe_names:
+        ner = nlp.create_pipe('ner')
+        nlp.add_pipe(ner)
+    else:
+        ner = nlp.get_pipe('ner')
+
+    for i in LABEL:
+        ner.add_label(i)   
+    
+    if model is None:
+        optimizer = nlp.begin_training()
+    else:
+        optimizer = nlp.entity.create_optimizer()
+
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
+    with nlp.disable_pipes(*other_pipes):  
+        for itn in range(n_iter):
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            batches = minibatch(TRAIN_DATA, size=compounding(4., 32., 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(texts, annotations, sgd=optimizer, drop=0.35,
+                           losses=losses)
+            print("epoch : ", itn, "    losses : ", losses)
+            wandb.log({'epoch': itn, 'loss': losses})
+
+    wandb.save("model-log.h5")
+    wandb.finish()
+
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.meta['name'] = new_model_name 
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
+
+        test_text = 'In year 2015 Wisumperuma completed secodary educaiton in Sri Lanka'
+        print("Loading model from", output_dir)
+        print("Testing sentence: ", test_text)
+        nlp2 = spacy.load(output_dir)
+        doc2 = nlp2(test_text)
+        for ent in doc2.ents:
+            print(ent.label_, ent.text)
+
+
+if __name__ == '__main__':
+    plac.call(main)
